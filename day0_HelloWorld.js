e23c2f2e4b28a7839ccdb6895a31d7c5e5035c97
Alveiro H <alveiro7@gmail.com>
2021-07-13 21:43:54 -0500

    :art: Day 0: Hello, World!

---
+++ day0_HelloWorld.js
@@ -0,0 +1,14 @@
+/* Task
+
+A greeting function is provided for you in the editor below. It has one parameter, parameterVariable). Perform the following tasks to complete this challenge:
+
+Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+Use console.log() to print the contents of  parameterVariable) (i.e., the argument passed to main).
+You've got this! */
+
+const greeting = (parameterVariable) => {
+    console.log("Hello world!")
+    console.log(`${parameterVariable}`)
+}
+
+greeting('Welcome to 10 days of JavaScript')
